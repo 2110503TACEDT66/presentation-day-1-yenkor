13e575ddc49b761e487abbd52d8c2429dfb4f673
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-01 20:00:54 +0700

    Setup database

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
