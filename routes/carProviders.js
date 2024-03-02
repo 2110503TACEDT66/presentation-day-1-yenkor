d61896e7ef97f349743426eeabf78a8e2d30a429
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-02 22:51:01 +0700

    Add rentingsRouter to carProviders route

--- routes/carProviders.js
+++ routes/carProviders.js
@@ -7,6 +7,8 @@ const {
   deleteCarProvider,
 } = require("../controllers/CarProviders");
 
+const rentingsRouter = require("./rentings");
+
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
 
@@ -14,6 +16,8 @@ router
   .route("/")
   .get(getCarProviders)
   .post(protect, authorize("admin"), createCarProvider);
+
+router.use("/:carProviderId/rentings", rentingsRouter);
 
 router
   .route("/:id")
