8ca57d0c529d11d4a9d193c5abc1a17461aa0c51
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-03-02 22:52:15 +0700

    Add authentication and authorization middleware to rentings routes

--- routes/rentings.js
+++ routes/rentings.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const {getRentings, getRenting, addRenting, updateRenting, deleteRenting} = require('../controllers/rentings');
+const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router({mergeParams: true});
-
-//const {protect, authorize} = require('../middleware/auth');
+;
 
 router.route('/')
-    .get(getRentings)       //add protect
-    .post(addRenting);      //add protect and authorize
+    .get(protect,getRentings)       
+    .post(protect,authorize('admin' ,'user'),addRenting);       
 router.route('/:id')
-    .get(getRenting)        //add protect
-    .put(updateRenting)     //add protect and authorize
-    .delete(deleteRenting); //add protect and authorize
+    .get(protect,getRenting)        
+    .put(protect, authorize('admin', 'user'),updateRenting)      
+    .delete(protect, authorize('admin', 'user'),deleteRenting);  
 
 module.exports = router;
