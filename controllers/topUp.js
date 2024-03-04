4488325edfd9a8c97961534027ac194bbfd075a0
Nithinan_ <nwattanasiri47@gmail.com>
2024-03-04 13:05:48 +0700

    Modified user can only topUp him/her self

--- controllers/topUp.js
+++ controllers/topUp.js
@@ -6,23 +6,64 @@ const User = require('../models/User');
 
 exports.topUp = async (req,res,next) => {
     try {
-        if (req.body.balance <= 0) {
-            return res.status(400).json({success: false, msg: 'The amount to be topped up must be greater than 0'});
-        }
-        const user = await User.findOneAndUpdate(
-            { email: req.body.email },  // Topup to target account with target account's email
-            { $inc: { balance: req.body.balance } }, // Increment the balance by the provided amount
-            { new: true, runValidators: true } 
-        );
+
+      if (req.user.role != 'admin') {  // If user's role is not admin They can only topUp them self.
+        const userId = req.user.id;
+        const balance = req.body.balance;
+
+        await addBalance(req, res, userId, balance);
+
+        // if (req.body.balance <= 0) {
+        //     return res.status(400).json({success: false, msg: 'The amount to be topped up must be greater than 0'});
+        // }
+        // const user = await User.findByIdAndUpdate(
+        //     userId,  // If user's role is not admin Topup by specify only balance amount
+        //     { $inc: { balance: req.body.balance } }, // Increment the balance by the provided amount
+        //     { new: true, runValidators: true } 
+        // );
     
-        if (!user) {
-          return res.status(400).json({ success: false , msg: `user with email: ${req.body.email} is not found`});
-        }
+        // if (!user) {
+        //   return res.status(400).json({ success: false , msg: `user with email: ${req.body.email} is not found`});
+        // }
     
-        res.status(200).json({ success: true, data: user });
+        // res.status(200).json({ success: true, data: user });
+
       } 
-      catch (err) {
-        res.status(400).json({ success: false });
+      else { // If user's role is admin They can topUp to any user.
+        const {userId, balance} = req.body;
+        
+        await addBalance(req, res, userId, balance);
       }
+    }
+    catch (error) {
+      console.log(error);
+      res.status(400).json({ success: false });
+    }
 };
+
+async function addBalance(req, res, userId, amount) {
+  try {
+    if (amount <= 0) {
+      return res.status(400).json({success: false, msg: 'The amount to be topped up must be greater than 0'});
+    }
+    const user = await User.findByIdAndUpdate(
+        userId,  
+        { $inc: { balance: amount } }, // Increment the balance by the provided amount
+        { new: true, runValidators: true } 
+    );
+
+    if (!user) {
+      return res.status(400).json({ success: false , msg: `user is not found`});
+    }
+
+    res.status(200).json({ success: true, data: user });
+  }
+  catch(error) {
+    console.log(error);
+    res.status(400).json({ success: false });
+  }
+}
+  
+
+
 
